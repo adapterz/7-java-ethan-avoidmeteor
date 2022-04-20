83f99cdfa1c7b4c71e42c170e5e7fab6c3116def
ethan <ethandeveloper2@gmail.com>
2022-04-20 15:14:31 +0900

    변수명 변경, 1클래스 1파일 수정

--- src/avoidMeteor.java
+++ src/avoidMeteor.java
@@ -1,56 +1,17 @@
 public class avoidMeteor {
     public static void main(String[] args) {
-        BattleSpaceShip a = new BattleSpaceShip();
-        a.attack();
+        BattleSpaceShip battleSpaceShip = new BattleSpaceShip();
+        battleSpaceShip.attack();
 
-        ScoutSpaceShip b = new ScoutSpaceShip();
-        b.move();
+        ScoutSpaceShip scoutSpaceShip = new ScoutSpaceShip();
+        scoutSpaceShip.move();
 
-        DefensiveSpaceShip c = new DefensiveSpaceShip();
-        c.defenseUp();
+        DefensiveSpaceShip defensiveSpaceShip = new DefensiveSpaceShip();
+        defensiveSpaceShip.defenseUp();
 
     }
 }
 
-class SpaceShip{
-    // 우주선 기본 능력치
-    int hp = 10;
-    int width = 10;
-    int height = 10;
-    int speed = 10;
-    void move() {
-        System.out.println("좌우 움직이기");
-    }
-}
 
-class BattleSpaceShip extends SpaceShip{
-    // 전투형 우주선 공격력 = powerspaceship
-    int power;
-    // 전투형 우주선 공격 기능
-    int attack(){
-        System.out.println("공격하기");
-        return power;
-    }
-}
 
-class ScoutSpaceShip extends SpaceShip{
-    // 정찰형 우주선 부스터 게이지 = boostGauge
-    int boostGauge;
-    // 정찰형 우주선 부스터 기능
-    int booster(){
-        System.out.println("부스터 사용하기");
-        speed = speed + boostGauge;
-        return speed;
-    }
-}
 
-class DefensiveSpaceShip extends SpaceShip{
-    // 방어형 우주선 방어력
-    int def;
-    // 방어형 우주선 방어력 추가 기능
-    int defenseUp(){
-        System.out.println("방어력 강화");
-        hp = hp + def;
-        return hp;
-    }
-}
